267ed75b80244a6fb2d866d07776628cd1835084
Nky237 <ezikenkemdilijane@gmail.com>
2023-03-14 13:36:55 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,9 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route index element= {<Land />} />
-    <Route path='' element={<RootLayout />}>
+      <Route path="/home" element={<Home />} />
+    {/* <Route path='' element={<RootLayout />}>
         <Route path="" element={<Navigate replace to="home" />} />
-        <Route path="home" element={<Home />} />
         <Route path='trend' element={<Trends />} />
         <Route path='artist' element={<Artist />} />
         <Route path='fav' element={<Favourite />} />
@@ -35,7 +35,7 @@ const router = createBrowserRouter(
         <Route path='release' element={<Release />} />
         <Route path='feed' element={<Feed />} />
         <Route path='event' element={<Event />} />
-    </Route>
+    </Route> */}
     </>
 
   )
